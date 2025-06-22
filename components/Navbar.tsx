4cccc4357e2c89776e0d056c9f34542d0d2fff3d
Iana <iana.li.timofeevna@gmail.com>
2025-06-22 19:04:20 +0200

    [update navbar]

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,54 +1,66 @@
-import React from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
-import { auth, signIn, signOut } from '@/auth';
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { auth, signIn, signOut } from "@/auth";
+import { BadgePlus, LogOut } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 const Navbar = async () => {
-
-    const session = await auth();
-    console.log("Session:", session);
+  const session = await auth();
+  console.log("Session:", session);
 
   return (
-    <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
-        <nav className="flex justify-between items-center">
-            <Link href="/">
-                <Image src="/logo.png" alt="Logo" width={80} height={60} />
-            </Link>
+    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center">
+        <Link href="/">
+          <Image src="/logo.png" alt="Logo" width={80} height={60} />
+        </Link>
 
-            <div className='flex items-center gap-5 text-black'>
-                {session && session.user ? (
-                    <>
-                        <Link href="/startup/create">
-                            <span>Create</span>
-                        </Link>
+        <div className="flex items-center gap-5 text-black">
+          {session && session.user ? (
+            <>
+              <Link href="/startup/create">
+                <span className="max-sm:hidden">Create</span>
+                <BadgePlus className="size-6 text-primary sm:hidden" />
+              </Link>
 
-                        <form action={async () => { 
-                            "use server";
+              <form
+                action={async () => {
+                  "use server";
 
-                            await signOut({ redirectTo: '/'})
-                        }}>
-                            <button type="submit">Logout</button>
-                        </form>
+                  await signOut({ redirectTo: "/" });
+                }}
+              >
+                <button type="submit" className=" max:sm:mt-1.5">
+                  <span className="max-sm:hidden">Logout</span>
+                  <LogOut className="size-6 text-red-500 sm:hidden" />
+                </button>
+              </form>
 
-                        <Link href={`/user/${session?.id}`}>
-                            <span>{session?.user?.name}</span>
-                        </Link>
-                    </>
-                ) : (
-                    <>
-                        <form action={async () => {
-                            "use server";
+              <Link href={`/user/${session?.id}`}>
+                <Avatar className="size-8">
+                  <AvatarImage src={session?.user?.image || ""} alt={session?.user?.name || ""} />
+                  <AvatarFallback>{session?.user?.name?.charAt(0) || ""}</AvatarFallback>
+                </Avatar>
+              </Link>
+            </>
+          ) : (
+            <>
+              <form
+                action={async () => {
+                  "use server";
 
-                            await signIn('github')
-                        }}>
-                            <button type='submit'>Login</button>  
-                        </form>
-                    </>
-                )}
-            </div>
-        </nav>
+                  await signIn("github");
+                }}
+              >
+                <button type="submit">Login</button>
+              </form>
+            </>
+          )}
+        </div>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
